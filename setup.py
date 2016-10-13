1b8613c739c9ae2f6656fc80b7064be046490d69
Sean Dague <sean@dague.net>
2016-10-13 15:04:02 -0400

    update for 0.4.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='proliphix',
-    version='0.3.1',
+    version='0.4.0',
     description="API for Proliphix nt10e network thermostat.",
     long_description=readme + '\n\n' + history,
     author="Sean Dague",
